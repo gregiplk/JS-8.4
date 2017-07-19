bf0790067bf4896e71ee00d07c5646975f950153
Greg Izzy <izydorek.g@gmail.com>
2017-07-19 15:27:27 +0200

    dodawanie komentarzy JS

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js 8.4
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name); // Komentarz do linii 'alert'
+console.log('Hello, ' + name);
+
+// komentarz liniowy
+
+/*
+komentarz
+blokowy
+*/
